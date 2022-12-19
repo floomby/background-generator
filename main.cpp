a52ab7fb6b5a23eef77be59974b7ae7cdb312375
Josh Hoover <caboodlennm@gmail.com>
2022-12-18 22:51:42 -0600

    Split into chunks plus threaded png encoding

--- main.cpp
+++ main.cpp
@@ -4,20 +4,165 @@
 #include <cmath>
 #include <cstddef>
 #include <cstring>
+#include <fstream>
 #include <iostream>
+#include <memory>
+#include <mutex>
 #include <ostream>
 #include <span>
 #include <stdexcept>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <thread>
 #include <tuple>
 #include <vector>
-#include <fstream>
 
 #include <CL/cl.h>
 
 #include "libs/spng.h"
+
+const char *getErrorString(cl_int error) {
+  switch (error) {
+  // run-time and JIT compiler errors
+  case 0:
+    return "CL_SUCCESS";
+  case -1:
+    return "CL_DEVICE_NOT_FOUND";
+  case -2:
+    return "CL_DEVICE_NOT_AVAILABLE";
+  case -3:
+    return "CL_COMPILER_NOT_AVAILABLE";
+  case -4:
+    return "CL_MEM_OBJECT_ALLOCATION_FAILURE";
+  case -5:
+    return "CL_OUT_OF_RESOURCES";
+  case -6:
+    return "CL_OUT_OF_HOST_MEMORY";
+  case -7:
+    return "CL_PROFILING_INFO_NOT_AVAILABLE";
+  case -8:
+    return "CL_MEM_COPY_OVERLAP";
+  case -9:
+    return "CL_IMAGE_FORMAT_MISMATCH";
+  case -10:
+    return "CL_IMAGE_FORMAT_NOT_SUPPORTED";
+  case -11:
+    return "CL_BUILD_PROGRAM_FAILURE";
+  case -12:
+    return "CL_MAP_FAILURE";
+  case -13:
+    return "CL_MISALIGNED_SUB_BUFFER_OFFSET";
+  case -14:
+    return "CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST";
+  case -15:
+    return "CL_COMPILE_PROGRAM_FAILURE";
+  case -16:
+    return "CL_LINKER_NOT_AVAILABLE";
+  case -17:
+    return "CL_LINK_PROGRAM_FAILURE";
+  case -18:
+    return "CL_DEVICE_PARTITION_FAILED";
+  case -19:
+    return "CL_KERNEL_ARG_INFO_NOT_AVAILABLE";
+
+  // compile-time errors
+  case -30:
+    return "CL_INVALID_VALUE";
+  case -31:
+    return "CL_INVALID_DEVICE_TYPE";
+  case -32:
+    return "CL_INVALID_PLATFORM";
+  case -33:
+    return "CL_INVALID_DEVICE";
+  case -34:
+    return "CL_INVALID_CONTEXT";
+  case -35:
+    return "CL_INVALID_QUEUE_PROPERTIES";
+  case -36:
+    return "CL_INVALID_COMMAND_QUEUE";
+  case -37:
+    return "CL_INVALID_HOST_PTR";
+  case -38:
+    return "CL_INVALID_MEM_OBJECT";
+  case -39:
+    return "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR";
+  case -40:
+    return "CL_INVALID_IMAGE_SIZE";
+  case -41:
+    return "CL_INVALID_SAMPLER";
+  case -42:
+    return "CL_INVALID_BINARY";
+  case -43:
+    return "CL_INVALID_BUILD_OPTIONS";
+  case -44:
+    return "CL_INVALID_PROGRAM";
+  case -45:
+    return "CL_INVALID_PROGRAM_EXECUTABLE";
+  case -46:
+    return "CL_INVALID_KERNEL_NAME";
+  case -47:
+    return "CL_INVALID_KERNEL_DEFINITION";
+  case -48:
+    return "CL_INVALID_KERNEL";
+  case -49:
+    return "CL_INVALID_ARG_INDEX";
+  case -50:
+    return "CL_INVALID_ARG_VALUE";
+  case -51:
+    return "CL_INVALID_ARG_SIZE";
+  case -52:
+    return "CL_INVALID_KERNEL_ARGS";
+  case -53:
+    return "CL_INVALID_WORK_DIMENSION";
+  case -54:
+    return "CL_INVALID_WORK_GROUP_SIZE";
+  case -55:
+    return "CL_INVALID_WORK_ITEM_SIZE";
+  case -56:
+    return "CL_INVALID_GLOBAL_OFFSET";
+  case -57:
+    return "CL_INVALID_EVENT_WAIT_LIST";
+  case -58:
+    return "CL_INVALID_EVENT";
+  case -59:
+    return "CL_INVALID_OPERATION";
+  case -60:
+    return "CL_INVALID_GL_OBJECT";
+  case -61:
+    return "CL_INVALID_BUFFER_SIZE";
+  case -62:
+    return "CL_INVALID_MIP_LEVEL";
+  case -63:
+    return "CL_INVALID_GLOBAL_WORK_SIZE";
+  case -64:
+    return "CL_INVALID_PROPERTY";
+  case -65:
+    return "CL_INVALID_IMAGE_DESCRIPTOR";
+  case -66:
+    return "CL_INVALID_COMPILER_OPTIONS";
+  case -67:
+    return "CL_INVALID_LINKER_OPTIONS";
+  case -68:
+    return "CL_INVALID_DEVICE_PARTITION_COUNT";
+
+  // extension errors
+  case -1000:
+    return "CL_INVALID_GL_SHAREGROUP_REFERENCE_KHR";
+  case -1001:
+    return "CL_PLATFORM_NOT_FOUND_KHR";
+  case -1002:
+    return "CL_INVALID_D3D10_DEVICE_KHR";
+  case -1003:
+    return "CL_INVALID_D3D10_RESOURCE_KHR";
+  case -1004:
+    return "CL_D3D10_RESOURCE_ALREADY_ACQUIRED_KHR";
+  case -1005:
+    return "CL_D3D10_RESOURCE_NOT_ACQUIRED_KHR";
+  default:
+    return "Unknown OpenCL error";
+  }
+}
 
 std::byte safeSubtract(std::byte a, std::byte b) {
   int ia = (int)a;
@@ -528,13 +673,95 @@ int oldMain(int argc, char **argv) {
   return 0;
 }
 
-#define MAX_SOURCE_SIZE (0x100000)
+class Chunk : public std::enable_shared_from_this<Chunk> {
+  float *data;
+  size_t dimension;
+  size_t offset[2];
+  std::mutex mutex;
+  std::string swapFileName;
+
+public:
+  Chunk(size_t dimension, std::array<size_t, 2> offset) {
+    assert(offset[0] % dimension == 0);
+    assert(offset[1] % dimension == 0);
+    this->dimension = dimension;
+    this->offset[0] = offset[0];
+    this->offset[1] = offset[1];
+    data = new float[dimension * dimension * 3];
+    std::memset(data, 0, dimension * dimension * 3 * sizeof(float));
+    swapFileName = std::to_string(offset[0] / dimension) + "_" +
+                   std::to_string(offset[1] / dimension);
+  }
+  ~Chunk() { delete[] data; }
+  std::thread dumpPNG() {
+    std::string filename = "chunk" + swapFileName + ".png";
+    const auto self = shared_from_this();
+    std::thread t([self, filename] {
+      std::scoped_lock lock(self->mutex);
+      ImageWriter writer;
+      Image image =
+          imageFromFloats(self->data, self->dimension, self->dimension);
+      writer.writePNG(filename, image);
+      std::cout << "Wrote " << filename << std::endl;
+    });
+    return t;
+  }
+  Chunk(const Chunk &) = delete;
+  Chunk &operator=(const Chunk &) = delete;
+  Chunk(Chunk &&) = delete;
+  Chunk &operator=(Chunk &&) = delete;
+
+  void swapOutToDisk() {
+    if (mutex.try_lock()) {
+      std::ofstream file(swapFileName, std::ios::binary);
+      file.write((char *)data, dimension * dimension * 3 * sizeof(float));
+      file.close();
+      delete[] data;
+    } else {
+      std::cout << "Failed to lock mutex (aborting): " << swapFileName
+                << std::endl;
+    }
+  }
+
+  void swapInFromDisk() {
+    data = new float[dimension * dimension * 3];
+    std::ifstream file(swapFileName, std::ios::binary);
+    file.read((char *)data, dimension * dimension * 3 * sizeof(float));
+    file.close();
+    mutex.unlock();
+  }
+
+  void generate(const cl_context &context, const cl_command_queue &queue, const cl_kernel &kernel, const cl_mem &outBuffer) {
+    if (!mutex.try_lock()) {
+      std::cout << "Failed to lock mutex (aborting): " << swapFileName << std::endl;
+      return;
+    }
+
+    std::cout << "Generating " << swapFileName << std::endl;
+
+    cl_int ret{CL_SUCCESS};
+
+    size_t globalWorkSize[2] = {dimension, dimension};
+    size_t localWorkSize[2] = {16, 16};
+
+    ret = clEnqueueNDRangeKernel(queue, kernel, 2, this->offset, globalWorkSize, localWorkSize, 0, NULL, NULL);
+    if (ret != CL_SUCCESS) {
+      std::cout << "Error enqueuing kernel" << std::endl;
+      throw std::runtime_error("Error enqueuing kernel");
+    }
+
+    ret = clEnqueueReadBuffer(queue, outBuffer, CL_TRUE, 0, dimension * dimension * 3 * sizeof(float), data, 0, NULL, NULL);
+    if (ret != CL_SUCCESS) {
+      std::cout << "Error reading buffer: " << getErrorString(ret) << std::endl;
+      // Get the error message
+      throw std::runtime_error("Error reading buffer");
+    }
+
+    mutex.unlock();
+  }
+};
 
 int main(int argc, char const *argv[]) {
-  const size_t dim = 10240;
-
-  const size_t size = dim * dim * 3;
-
   // Read in the shader
   std::ifstream t("gen.ocl");
   std::string source_str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
@@ -547,26 +774,30 @@ int main(int argc, char const *argv[]) {
   cl_uint ret_num_devices;
   cl_uint ret_num_platforms;
   cl_int ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
-  ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &ret_num_devices);
+  if (ret != CL_SUCCESS) {
+    std::cout << "Error getting platform IDs" << std::endl;
+    throw std::runtime_error("Error getting platform IDs");
+  }
 
-  // Create an OpenCL context
+  ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &ret_num_devices);
+  if (ret != CL_SUCCESS) {
+    std::cout << "Error getting device IDs" << std::endl;
+    throw std::runtime_error("Error getting device IDs");
+  }
+
   cl_context context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
 
   // Create a command queue
-  cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
-
-  // Create memory buffers on the device for each vector
-  cl_mem out_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, size * sizeof(float), NULL, &ret);
+  cl_command_queue command_queue =
+      clCreateCommandQueue(context, device_id, 0, &ret);
   if (ret != CL_SUCCESS) {
-    std::cout << "Error creating buffer" << std::endl;
-    throw std::runtime_error("Error creating buffer");
+    std::cout << "Error creating command queue" << std::endl;
+    throw std::runtime_error("Error creating command queue");
   }
 
-  // Create a program from the kernel source
   const size_t sourceSize = strlen(source);
   cl_program program = clCreateProgramWithSource(context, 1, (const char **)&source, &sourceSize, &ret);
 
-  // Build the program
   ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
   if (ret != CL_SUCCESS) {
     std::cout << "Error building program" << std::endl;
@@ -579,39 +810,40 @@ int main(int argc, char const *argv[]) {
     throw std::runtime_error("Error building program");
   }
 
-  // Create the OpenCL kernel
   cl_kernel kernel = clCreateKernel(program, "perlin", &ret);
   if (ret != CL_SUCCESS) {
     std::cout << "Error creating kernel" << std::endl;
     throw std::runtime_error("Error creating kernel");
   }
 
-  ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&out_mem_obj);
+  cl_mem outBuffer = clCreateBuffer(
+      context, CL_MEM_WRITE_ONLY, 2048 * 2048 * 3 * sizeof(float), NULL, &ret);
+  if (ret != CL_SUCCESS) {
+    std::cout << "Error creating buffer" << std::endl;
+    throw std::runtime_error("Error creating buffer");
+  }
+
+  ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), &outBuffer);
   if (ret != CL_SUCCESS) {
     std::cout << "Error setting kernel arg" << std::endl;
     throw std::runtime_error("Error setting kernel arg");
   }
 
-  size_t localWorkSize[2] = {16, 16};
-  size_t globalWorkSize[2] = {dim, dim};
-  ret = clEnqueueNDRangeKernel(command_queue, kernel, 2, NULL, globalWorkSize, localWorkSize, 0, NULL, NULL);
-  if (ret != CL_SUCCESS) {
-    std::cout << "Error enqueueing kernel" << std::endl;
-    throw std::runtime_error("Error enqueueing kernel");
+  std::vector <std::thread> threads;
+
+  auto chunk = std::make_shared<Chunk>(2048, std::array<size_t, 2>({0UL, 0UL}));
+  chunk->generate(context, command_queue, kernel, outBuffer);
+  threads.push_back(chunk->dumpPNG());
+
+  chunk = std::make_shared<Chunk>(2048, std::array<size_t, 2>({2048UL, 0UL}));
+  chunk->generate(context, command_queue, kernel, outBuffer);
+  threads.push_back(chunk->dumpPNG());
+
+  clReleaseMemObject(outBuffer);
+
+  for (auto &thread : threads) {
+    thread.join();
   }
 
-  float *out = new float[size];
-  ret = clEnqueueReadBuffer(command_queue, out_mem_obj, CL_TRUE, 0, size * sizeof(float), out, 0, NULL, NULL);
-  if (ret != CL_SUCCESS) {
-    std::cout << "Error reading buffer" << std::endl;
-    throw std::runtime_error("Error reading buffer");
-  }
-
-  ImageWriter writer;
-  Image image = imageFromFloats(out, dim, dim);
-  std::cout << "Writing image" << std::endl;
-  writer.writePNG("noise.png", image);
-
-  delete[] out;
   return 0;
 }
